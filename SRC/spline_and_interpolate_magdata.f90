35d06d0aa0385ef3cddd1c4e2a299db5c3a8e40a
Christopher Albert <albert@tugraz.at>
2025-01-18 01:38:01 +0100

    Clean

--- SRC/spline_and_interpolate_magdata.f90
+++ SRC/spline_and_interpolate_magdata.f90
@@ -115,7 +115,7 @@
   double precision, intent(inout) :: s,psi,theta
   double precision, intent(out) :: q,dq_ds,C_norm,dC_norm_ds,sqrtg,bmod,dbmod_dtheta, &
                       R,dR_ds,dR_dtheta,Z,dZ_ds,dZ_dtheta,G,dG_ds,dG_dtheta
-  integer :: ir,it,k,km1,ibeg,iend
+  integer :: it,k,km1,ibeg,iend
   double precision :: dtheta
 !
   if(inp_label.eq.1) then
